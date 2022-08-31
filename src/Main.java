3c3f7cbd01ccedbef9923e8ee3808f60c16df8b8
akaps98 <fc0302@naver.com>
2022-08-31 16:07:12 +0700

    updated Main class

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,8 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        welcomeScreen();
-        orderManagementSystem();
-    }
-
-    public static void orderManagementSystem() {
-        Scanner input = new Scanner(System.in);
-        while(true) {
-            int checkPerson;
-            System.out.println("Welcome to the order management system.");
-            System.out.println("If you are customer, enter 1.");
-            System.out.println("If you are administrator, enter 2.");
-
-            checkPerson = input.nextInt();
-
-            if(checkPerson == 1) { // if he/she is customer,
-
-            } else { // if he/she is admin,
-
-            }
-        }
-    }
-
-    public static void welcomeScreen() {
-        System.out.println("COSC2081 GROUP ASSIGNMENT");
-        System.out.println("STORE ORDER MANAGEMENT SYSTEM");
-        System.out.println("Instructor: Mr. Minh Vu ");
-        System.out.println("Group 5");
-        System.out.println("s3916884, Junsik Kang");
-        System.out.println("s3926977, Doan Thien Di");
-        System.out.println("s3864235, Seungmin Lee");
-        System.out.println("s3914412, Nguyen Duong Truong Thinh");
+    public static void main(String[] args) throws IOException {
+        ManagementSystem.orderManagementSystem();
     }
 }
