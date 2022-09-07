1c725707a0afd708188c478590cb150c2d21cf53
akaps98 <fc0302@naver.com>
2022-09-07 23:39:24 +0700

    updated Admin class

--- src/StoreOrderManagementSystem/Admin.java
+++ src/StoreOrderManagementSystem/Admin.java
@@ -2,10 +2,23 @@ package StoreOrderManagementSystem;
 
 public class Admin {
     private String username = "group5";
-    private String password = "01234456";
+    private String password = "123456";
 
     public Admin(){};
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
