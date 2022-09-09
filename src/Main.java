60a039fee6cf378b1cbec11d1ed0aa4cf4a1bb80
Truong-Thinh14 <s3914412@rmit.edu.vn>
2022-09-09 22:05:31 +0700

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        ManagementSystem.orderManagementSystem();
             Order.getOrderByCustomerID();
+//            Order.changeStatus();
     }
 }
--- src/StoreOrderManagementSystem/Admin.java
+++ src/StoreOrderManagementSystem/Admin.java
@@ -1,10 +1,8 @@
 package StoreOrderManagementSystem;
-
+import java.util.Scanner;
 public class Admin {
     private String username = "group5";
-    private String password = "123456";
-
-    public Admin() {};
+    private String password = "cosc2081";
 
     public String getUsername() {
         return username;
@@ -21,4 +19,25 @@ public class Admin {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public static void Admin(String[] args) {
+        new Admin();
+
+        Scanner input = new Scanner(System.in);
+
+        String username, password;
+
+        System.out.print("Enter the username: ");
+        username = input.nextLine();
+
+        System.out.print("Enter the password: ");
+        password = input.nextLine();
+
+        if (username.equals("admingroup5") && (password.equals("cosc2081"))) {
+            System.out.println("Welcome admin group 5.");
+        } else {
+            System.out.println("Wrong username or password!");
+        }
+    }
 }
+
